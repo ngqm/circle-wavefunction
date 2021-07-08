d26519bd2564218103dc5590964c957fe3f8a5b4
Quang Minh Nguyen <minh_nguyen@kaist.ac.kr>
2021-07-08 13:55:00 +0900

    typo

--- circle-wavefunction.py
+++ circle-wavefunction.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt 
 plt.style.use('fivethirtyeight')
 
-import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
 
 '''
@@ -23,7 +22,7 @@ x, y 	= np.sin(X), np.cos(X)
 
 # figure setup
 fig = plt.figure(figsize=(18,6))
-plt.suptitle('Energy eigenfunctions of a free particle on a circle')
+plt.suptitle('Energy eigenfunctions of a free electron on a circle')
 
 ax_re, ax_im 		= [None]*4, [None]*4
 lines_re, lines_im 	= [None]*4, [None]*4
@@ -34,7 +33,6 @@ for n in range(4):
 	ax_re[n].set_ylim3d([-1.25, 1.25])
 	ax_re[n].set_zlim3d([-1.25, 1.25])
 	ax_re[n].set_zlabel('$Re(\psi_{}(x,t))$'.format(n), labelpad=10)
-	#ax[n].dist = 12 # default is 10
 
 	ax_im[n] = fig.add_subplot(int(245 + n), projection='3d')
 	ax_im[n].set_xlim3d([-1.25, 1.25])
